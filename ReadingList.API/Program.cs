573beb21edc2db122d7030df9c3e9398ca6bb7b9
Jarek Blady <jarek.blady97@gmail.com>
2022-12-05 23:44:32 +0100

    Add Entities

--- ReadingList.API/Program.cs
+++ ReadingList.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReadingList.API.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<BookDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BookConnectionString")));
+
+
+
+
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
